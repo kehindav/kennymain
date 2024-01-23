08718da3fcd63fc1db4571ae12e83de3073fdf90
kenny <kehindavaws@gmail.com>
2024-01-22 21:59:22 -0500

    adding terraform filess

--- simple/main.tf
+++ simple/main.tf
@@ -13,5 +13,7 @@ terraform {
 provider "aws" {
   region = "us-east-1" 
   # region  = var.aws_region
+  access_key = "AKIAV7QUWO6FKKTFP36J"
+  secret_key = "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"
   
 }
